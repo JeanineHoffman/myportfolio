aa3346a9551cf9ecdd33c33ee1a5c7e8cf7f0ad7
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-02-15 11:48:47 -0800

    working on JS

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 console.log('JS connected');
 // fill in psuedo code
 
-
-
+// nav buttons
+function navBtn(){
+  $('.navbtn').on('click', function(event){
+    if ((event) === 1) {
+       $('.bio');
+    }
+    else{
+      if ((event) === 2){
+        return $('.js-projects');
+      }
+      else{
+        if ((event) === 3){
+          return $('.contact');
+        }
+        else{
+          if ((event) === 4){
+            return $('.triangle');
+          }
+        }
+      }
+    }
+  });
+}
 
 
 function projects(){
